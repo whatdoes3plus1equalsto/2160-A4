987e1667ad38068d896d96093010ecb33e04ceeb
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2022-08-08 06:47:10 -0500

    Q1: initPool completed and commented

--- ObjectManager.c
+++ ObjectManager.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 
 #include "ObjectManager.h"
 
-static unsigned char *buffer1;  //double buffer
-static unsigned char *buffer2;  //double buffer
+static uchar *buffer1;  //double buffer
+static uchar *buffer2;  //double buffer
 
-static unsigned char *bufferCurr; //the buffer in use
+static uchar *bufferCurr = NULL; //point to the buffer in use
 
-typedef struct NODE{
-    //storing object data
-    int numBytes;   //size
+typedef struct NODE{ //storing object data
+
+    int numBytes;   //size of object
     int startAddr;  //starting address in the buffer
     int ref;    //id
     int count;  //times being called
     Node *next; //next node
+
 }Node;
 
 static Node *root = NULL; //the top of the linked list
 
-static int freePtr; //point to the insert address
+static int insertPtr = 0; //point to the insert address
 
-static int numOfBlocks; //number of blocks
+static int numOfBlocks = 0; //number of blocks
 
 static void validate(){
     //invarient
@@ -30,6 +32,13 @@ static void validate(){
 
     #endif
 }
+
+static void compact(){
+    //Initiate garbage collection
+
+}
+
+////Interface////
 
 Ref insertObject( ulong size ){
     //Request a block of memory of given size from the object manager
@@ -49,6 +58,17 @@ void dropReference( Ref ref ){
 
 void initPool(){
     //Initialize the object manager upon starting
+    numOfBlocks = 1;
+    root = NULL;
+    insertPtr = 0;
+    buffer1 = (uchar *)malloc(sizeof(MEMORY_SIZE));
+    buffer2 = (uchar *)malloc(sizeof(MEMORY_SIZE));
+
+    bufferCurr = buffer1;
+    printf("\nSize of the buffer:  %d\n", MEMORY_SIZE);
+
+    //postcondition
+    validate();
 }
 
 void destroyPool(){
@@ -57,8 +77,4 @@ void destroyPool(){
 
 void dumpPool(){
     //Print (to stdout) info about each object that is currently allocated including its id, start address, and size
-}
-
-static void compact(){
-    //Initiate garbage collection
 }

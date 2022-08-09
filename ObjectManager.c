9b2b4a6ca98f79dac37ab474f1af90e3242f44ce
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2022-08-08 19:17:26 -0500

    Q1: insertObject() testing

--- ObjectManager.c
+++ ObjectManager.c
@@ -8,25 +8,25 @@
 static uchar *buffer1;  //double buffer
 static uchar *buffer2;  //double buffer
 
-static uchar *bufferCurr = NULL; //point to the buffer in use
+static uchar *bufferCurr; //point to the buffer in use
 
 typedef struct NODE{ //storing object data
 
-    int numBytes;   //size of object
-    int startAddr;  //starting address in the buffer
-    int ref;    //id
+    ulong numBytes;   //size of object
+    ulong startAddr;  //starting address in the buffer
+    Ref ref;    //id
     int count;  //times being called
     Node *next; //next node
 
 }Node;
 
-static Node *root = NULL; //the top of the linked list
+static Node *head; //the top of the linked list
 
-static int insertPtr = 0; //point to the insert address
+static ulong insertPtr; //point to the insert address
 
-static int numOfBlocks = 0; //number of blocks
+static int numOfBlocks; //number of blocks
 
-static void validate(){
+static void validate(){ 
     //invarient
     #ifndef NDEBUG
 
@@ -42,6 +42,79 @@ static void compact(){
 
 Ref insertObject( ulong size ){
     //Request a block of memory of given size from the object manager
+    //return the reference to the object inserted
+
+    //precondition
+    validate();
+    assert(size >= 0);
+    if(size < 0){
+        printf("The size should be a positive integer");
+
+        return NULL_REF;
+    }
+    
+    if(size <= MEMORY_SIZE){
+        if(insertPtr + size > MEMORY_SIZE){
+
+            //invarient
+            assert(insertObject > 0);
+
+            //run garbage collector
+            compact();
+        }
+
+        if(insertPtr + size <= MEMORY_SIZE){
+            //making the object Node
+            Node *newNode = NULL;
+            newNode->ref = numOfBlocks++;
+            newNode->count = 1;
+            newNode->numBytes = size;
+            newNode->startAddr = insertPtr;
+            newNode->next = NULL;
+            insertPtr += size;
+
+            if(head == NULL){
+                //if the list is empty
+                head = newNode;
+
+                //postcondition
+                validate();
+
+                return newNode->ref;
+            }else{
+                //if the list isnt empty
+                Node *curr = head;   //iterator
+                Node *prev = NULL;   //iterator
+
+                while(curr != NULL){
+                    prev = curr;
+                    curr = curr->next;
+                }
+
+                prev->next = newNode;
+
+                //postcondition
+                validate();
+
+                return newNode->ref;
+            }   
+            
+        }else{
+            printf("No space available for inserting this object\n");
+            printf("The space required: %d\nThe space available: %d\n", size, MEMORY_SIZE-insertPtr);
+            //postcondition
+            validate();
+
+            return NULL_REF;
+        }
+    }else{
+        printf("The size requested is way bigger than the size of the buffer\n");
+        
+        //postcondition
+        validate();
+
+        return NULL_REF;
+    }
 }
 
 void *retrieveObject( Ref ref ){
@@ -59,12 +132,12 @@ void dropReference( Ref ref ){
 void initPool(){
     //Initialize the object manager upon starting
     numOfBlocks = 1;
-    root = NULL;
+    head = NULL;
     insertPtr = 0;
     buffer1 = (uchar *)malloc(sizeof(MEMORY_SIZE));
     buffer2 = (uchar *)malloc(sizeof(MEMORY_SIZE));
-
     bufferCurr = buffer1;
+
     printf("\nSize of the buffer:  %d\n", MEMORY_SIZE);
 
     //postcondition
